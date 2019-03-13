6b8befb662ab0628b44083a0efa0b18460524dff
Albano Correia <albanoacorreia@gmail.com>
2019-03-13 22:00:30 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-132926178-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-132926178-1');
+    </script>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="icon" type="image/png" href="assets/img/favico.png"/>
+        <title>Dickie Bird</title>
+        <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="nav-bar flex">
+      <ul class="main-nav">
+          <li><a href="#">Bio</a></li>
+          <li><a href="#">Statistics</a></li>
+          <li><a href="#">Pictures</a></li>
+          <li><a href="#">Location</a></li>
+      </ul>
+      <ul class="social-nav flex">
+          <li> <a href="#"><img  src="assets/img/facebook-icon.png" alt="Facebook Logo"/></a></li>
+          <li><a href="#"> <img  src="assets/img/insta-icon.png" alt="Instagram Logo"/></a></li>
+          <li><a href="#"> <img  src="assets/img/twitter-ivon.png" alt="Twitter Logo"/></a></li>
+      </ul>
+  </div><!-- /.nav-bar  flex-->
+
+    <div class="container">
+        <header>
+            <div class="hero">
+                <div class="hero-logo">
+                    <img src="assets/img/logo-pic.png" alt="Dickie Bird Logo"/>
+                </div><!-- /.hero-logo -->
+                <div class="hero-text">
+                    <h1>Who is <span>Dickie Bird</span></h1>
+                </div><!-- /.hero-text -->
+            </div><!-- /.hero -->
+        </header>
+
+        <div class="content flex">
+            <div class="main-content">
+                <h3>Bio</h3>
+                <p>Dickie Bird's white cap, twitching shoulders and forearm stretch became as much a part of the day out as the battle between bat and ball. He seemed to attract minor incident, without ever allowing the day to be soured by controversy. Burst pipes, reflecting windscreens and bomb scares all conspired to trouble him, but each impostor was met with humour and Yorkshire-bred common sense. Notorious for being an early Bird (he once made it to The Oval at 6am, so nervous was he about being late and was five-hours early to meet the Queen on one occasion), and plagued by bad weather he made the transition from cricketing figure to something close to a national institution.</p>
+
+                <p class="sep">A favourite among the public who took to his idiosyncrasies, Bird was equally admired and respected by the players. He diffused many a situation that a lesser umpire may have allowed to escalate, typically with common sense and good humour. Most importantly, he simply made few mistakes. "They all rated me the best: Sobers, Richards, Lillee and Botham. That means a lot I can tell you," he admitted.</p>
+
+
+
+                <p>Harold "Dickie" Bird was born in Barnsley in April 1933. 5'10" and a right-hand batsman, Bird played in a Barnsley side that included Geoff Boycott and the journalist Michael Parkinson, with whom he remained friends. A good enough player to have represented Yorkshire during their period of dominance (debuting in 1956 against Scotland), a regular first team place eluded him in such a strong side (his career best performance 181* against Glamorgan at Bradford in 1959 was rewarded with omission from the next game as the senior players returned from Test duty) and he moved to Leicestershire at the end of that year. He won his county cap in his first season, 1960, in which he topped 1,000 first-class runs, but later referred to the decision to leave Yorkshire as "the biggest mistake of my life".</p>
+
+                <p class="sep">Bird retired relatively early, aged just 32, and spent a few years coaching and playing as a club professional. He later admitted to regret that his playing career had not been more successful (first-class average: 20.71): "I just wish I'd have believed in myself as a batsman the way I do as an umpire," he told The Cricketer (November 1998) during his final match as an umpire. "I had the ability I can tell you. If you had compared me to Boycott in the nets you would have picked me as the Test player. Ray Illingworth said I played as straight as anyone he'd ever seen. Umpiring has been good to me, but it is the second-best thing to playing."</p>
+
+
+
+                <p>Bird officiated his first game in 1970, with his first Test coming against New Zealand at Leeds in 1973. In 1992 he stood in Zimbabwe's inaugural Test as the first ICC panel umpire. A guard of honour by the players and a standing ovation from the crowd as England took on India in 1996 marked his final Test, at his beloved Lord's. Bird, always an emotional man, was reduced to tears. In all he officiated in 66 Tests and 69 One-Day Internationals, overtaking Frank Chester's record (of 48 Tests) in Zimbabwe in 1996. His final first-class game was at Headingley, between Yorkshire and Warwickshire in 1998.
+                </p>
+                <p class="sep">Bird was awarded the MBE in June 1986 in the Queen's Birthday Honours List ("It means more to me than my life," he commented) and became a frequent tea-time visitor at the Palace, apparently attending more than 20 times. He gained other remarkable friends, including former Prime Minister John Major, billionaire cricket fanatic John Paul Getty and the thriller writer Stephen King. "I wish I'd had a family. That's where I missed out in life," he lamented on his retirement but his oft-repeated phrase was that he was "married to the game."</p>
+
+
+
+                <p>Bird's real legacy will be top-quality umpiring. Calm, despite his nervous disposition; consistent, despite his erratic body movements; and unimpeachably impartial despite his obvious love for all things Yorkshire and England, Bird added to the enjoyment of the spectators without ever detracting from the cricket. Maybe a little reluctant to give lbw decisions, (he would argue "the Laws state I have to be certain"), he has expressed reservations about the marginalisation of umpires by technology in recent times.</p>
+
+                <p>A busy retirement, in which he revels in the new role of a media personality, has seen the ubiquitous Bird appear on quiz and chat shows, embark on a speaking tour where his routine of anecdotes provokes great amusement and improve upon each telling. He has produced several books, including My Autobiography with Keith Lodge, the biggest selling sports book of all time and the follow-up White Cap and Bails, another best-seller.</p>
+            </div><!-- /.main-content -->
+
+            <div class="sidebar">
+                <div class="stats">
+                    <h3>Id</h3>
+                    <ul>
+                        <li>Full Name: Harold Dennis Bird</li>
+                        <li>Born: April 19, 1933, Barnsley, Yorkshire</li>
+                        <li>Education: Raley School, Barnsley</li>
+                        <li>eams: Leicestershire, Yorkshire</li>
+                        <li>Nickname: Dickie</li>
+                        <li>Batting Style: Right-hand</li>
+                        <li>Bowling Style: Right-arm Offbreak</li>
+                        <li>Other: Umpire</li>
+                    </ul>
+                </div><!-- /.stats -->
+                <div class="stats">
+                    <h3>Playing Statistics</h3>
+                    <ul>
+                        <li>First-class Matches: 93</li>
+                        <li>Innings: 170</li>
+                        <li>Not Outs: 10</li>
+                        <li>Runs: 3314</li>
+                        <li>Highest Score: 181*</li>
+                        <li>Average: 20.71</li>
+                        <li>Balls: 48</li>
+                        <li>Runs: 22</li>
+                        <li>Wickets: 0</li>
+                    </ul>
+                </div><!-- /.stats -->
+                <div class="stats">
+                    <h3>Umpiring Statistics</h3>
+                    <ul>
+                        <li>Number of Test Matches: 66</li>
+                        <li>Test Debut: England v New Zealand at Leeds, Jul 5-10, 1973</li>
+                        <li>Last Test: England v India at Lord's, Jun 20-24, 1996</li>
+                        <li>Number of ODI Matches: 69</li>
+                        <li>ODI Debut: England v New Zealand at Manchester, Jul 20, 1973</li>
+                        <li>Last ODI: England v West Indies at The Oval, May 26, 1995</li>
+                    </ul>
+                </div><!-- /.stats -->
+            </div><!-- /.sidebar -->
+        </div><!-- /.content -->
+
+            <div class="pictures  flex">
+                <h3>Pictures</h3>
+                <ul>
+                    <li>
+                        <img src="assets/img/dickie-bird-statue.jpg" alt="Dickie Bird Statue"/>
+                    </li>
+                    <li>
+                        <img src="assets/img/dickie-bird-tribute.jpg" alt="Dickie Bird Tribute"/>
+                    </li>
+                    <li>
+                        <img src="assets/img/dickie-bird-statuee.jpg" alt="Dickie Bird Statue"/>
+                    </li>
+                    <li>
+                        <img src="assets/img/dickie-bird-posing.jpg" alt="Dickie Bird Posing"/>
+                    </li>
+                </ul>
+            </div><!-- /.pictures -->
+
+        <div class="contact-form" id="contact">
+            <h3>Contact</h3>
+                <div class="form">
+                    <?php if (isset($_GET['thanks']) AND $_GET['thanks'] == 1): ?>
+                    <p>Thank you for your submission!</p>
+                    <?php endif; ?>
+                    <form action="contactengine.php" method="post">
+                        <input name="Name" type="text" placeholder="Your Name">
+                        <br>
+                        <input name="Email" type="email" placeholder="Email"><br>
+                        <p></p> <textarea name="Message" placeholder="Leave your comment..." rows="10" cols="30"></textarea>
+                        <br>
+                        <input type="submit">
+                    </form>
+                </div><!-- /.form -->
+        </div><!-- /.contact-form -->
+
+        <footer>
+            <div class="flex items-center">
+                <div class="site-logo">
+                    <img src="assets/img/dickie-bird-autograph.png" alt="Dickie Bird autograph"/>
+                </div><!-- /.site-logo -->
+                    <ul class="clubs flex">
+                        <li>
+                            <a href="#"><img src="assets/img/yccc.png" alt="Yorkshire Cricket Club"/></a>
+                        </li>
+                        <li>
+                            <a href="#"><img src="assets/img/icc.png" alt="International Cricket Club"/></a>
+                        </li>
+                        <li>
+                            <a href="#"><img src="assets/img/ecb.png" alt="England Cricket Club"/></a>
+                        </li>
+                    </ul>
+            </div><!-- /.flex items-center -->
+                <div class="copyright">
+                    <p>Copyright &copy; <a href="#">Albano Correia</a> 2019</p>
+                </div><!-- /.copyright -->
+        </footer>
+    </div>
+</body>
+</html>
